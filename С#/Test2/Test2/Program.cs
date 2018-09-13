1189fd824bad2ad0d0e2a40b4f51f0b751212564
Kamikoto <dan.mok999@yandex.ru>
2018-09-13 12:14:34 +0500

    С#

--- С#/Test2/Test2/Program.cs
+++ С#/Test2/Test2/Program.cs
@@ -5,23 +5,9 @@ namespace Slide01
     {
         static void Main()
         {
-            System.Console.WriteLine(Min(4, 2, 3));
-        }
-
-        private static string GetMinX(int a, int b, int c)
-        {
-            if (a != 0)
-            {
-                double x0 = (-b) / (2.0 * a);
-                return x0.ToString();
-            }
-
-            if (a == 0 && (c == 0 || c == 2))
-            {
-                return "a";
-            }
-
-            return "Impossible";
+            String a = Console.ReadLine();
+            char[] s = a.ToCharArray();
+            Array.Reverse(s);
         }
     }
 }

de754b7dc91f3a147eda9b52deb3fb600d9b62e7
Kamikoto <dan.mok999@yandex.ru>
2019-01-13 19:02:12 +0500

    PL4 Build

--- Python/PlayListStore_4.0/BUILD.bat
+++ Python/PlayListStore_4.0/BUILD.bat
@@ -1,7 +1,10 @@
-del misc.txt
-rmdir build
+pyinstaller --onefile --icon=icons\pls.ico --noconsole main.py
+del main.spec
+rmdir /s /q build
+mkdir dist\icons dist\gui
+copy style.css dist
+copy ExObjects.py dist
+copy data.pls dist
+copy gui dist\gui
+copy icons dist\icons
 rename dist build
-copy style.css build
-mkdir build\icons build\gui
-copy gui build\gui
-copy icons build\icons

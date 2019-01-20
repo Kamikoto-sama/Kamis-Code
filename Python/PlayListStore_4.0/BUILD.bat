0b8d56e3a479f0c294917efe7b04f4a05c6f0618
Kamikoto <dan.mok999@yandex.ru>
2019-01-20 14:09:38 +0500

    Rebuild

--- Python/PlayListStore_4.0/BUILD.bat
+++ Python/PlayListStore_4.0/BUILD.bat
@@ -1,4 +1,5 @@
 python db_generator.py 0
+rmdir /s /q build
 pyinstaller --onefile --icon=icons\pls.ico --noconsole main.py
 del main.spec
 rmdir /s /q build
@@ -7,6 +8,7 @@ copy style.css dist
 copy data.pls dist
 copy gui dist\gui
 copy icons dist\icons
-rename dist\main.exe "PlayListStore 4.0.exe"
+rename dist\main.exe "PlayListStore 4.exe"
 rename dist build
+del ..\..\Release\PLS4.rar
 start winrar a -r ..\..\Release\PLS4 build

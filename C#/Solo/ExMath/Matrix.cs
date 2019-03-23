2afd7505d8de7193de13784fff4829169b205277
Kamikoto <dan.mok999@yandex.ru>
2019-03-23 21:01:18 +0500

    Matrix

--- C#/Solo/ExMath/Matrix.cs
+++ C#/Solo/ExMath/Matrix.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace ExMath
 {
@@ -16,46 +17,68 @@ namespace ExMath
             get => Values[i, j];
             set => Values[i, j] = value;
         }
-        
-        public Matrix(int rows, int columns)
-        {
-            Values = new double[rows, columns];
-        }
 
         public Matrix(double[,] values) => Values = values;
+
+        public Matrix(int rowCount, int columnCount, double value=0)
+        {
+            var values = new double[rowCount, columnCount];
+            Values = GetNewValues(values, (i, j) => value);
+        }
+        
+        public Matrix Clone() => new Matrix((double[,])Values.Clone());
+
+        public void PrintInLine()
+        {
+            Console.Write("( ");
+            for (var i = 0; i < RowCount; i++)
+            {
+                Console.Write("{ ");
+                for (var j = 0; j < ColumnCount; j++)
+                    Console.Write(Values[i, j] + " ");
+                Console.Write("} ");
+            }
+            Console.Write(")");
+        }
 
         public static Matrix operator +(Matrix left, Matrix right)
         {
             var rows = left.RowCount;
             var columns = left.ColumnCount;
-            if(rows != right.RowCount
-               && columns != right.ColumnCount)
-                throw new Exception("Sizes aren't equals");
+            if(rows != right.RowCount || columns != right.ColumnCount)
+                throw new Exception("Matrix sizes aren't equal");
             
-            var newValues = GetNewValues(left, (i, j) => left[i, j] + right[i, j]);
+            var newValues = GetNewValues(left.Values, 
+                (i, j) => left[i, j] + right[i, j]);
             return new Matrix(newValues);
         }
 
         public static Matrix operator *(Matrix matrix, double value)
         {
-            var newValues = GetNewValues(matrix, (i, j) => matrix[i, j] * value);
+            var newValues = GetNewValues(matrix.Values, 
+                (i, j) => matrix[i, j] * value);
             return new Matrix(newValues);
         }
 
         public static Matrix operator *(double value, Matrix matrix) => matrix * value;
 
-        public static Matrix operator *(Matrix right, Matrix left)
+        public static Matrix operator *(Matrix left, Matrix right)
         {
             if(right.ColumnCount != left.RowCount)
-                throw new Exception("right.ColumnCount must be equal left.RowCount");
+                throw new Exception("left.ColumnCount must " +
+                                    "be equal to right.RowCount");
 
-            var newValues = new double[right.RowCount, right.ColumnCount];
-            var cellValue = .0;
-            for (int i = 0; i < right.RowCount; i++)
-            for (int j = 0; j < right.ColumnCount; j++)
+            var newValues = new double[right.RowCount, left.ColumnCount];
+            for (int i = 0, j; i < right.RowCount * left.ColumnCount; i++)
             {
-                cellValue += right[i, j] * left[j, i]              
+                var cellValue = 0.0;
+                for (j = 0; j < right.ColumnCount; j++)
+                    cellValue += left[i / right.ColumnCount, j] * right[j, i % right.RowCount];
+
+                newValues[i / right.ColumnCount, i % right.ColumnCount] = cellValue;
             }
+            
+            return new Matrix(newValues);
         }
 
         public static Matrix operator /(Matrix matrix, double value)
@@ -64,14 +87,13 @@ namespace ExMath
         }
 
         public static Matrix operator /(double value, Matrix matrix) => matrix / value;
-
         
-        private static double[,] GetNewValues(Matrix matrix, 
+        private static double[,] GetNewValues(double[,] values, 
             Func<int, int, double> operation)
         {
-            var newValues = new double[matrix.RowCount, matrix.ColumnCount];
-            for (var i = 0; i < matrix.RowCount; i++)
-            for (var j = 0; j < matrix.ColumnCount; j++)
+            var newValues = new double[values.GetLength(0), values.GetLength(1)];
+            for (var i = 0; i < values.GetLength(0); i++)
+            for (var j = 0; j < values.GetLength(1); j++)
                 newValues[i, j] = operation(i, j);
             return newValues;
         }
@@ -84,6 +106,24 @@ namespace ExMath
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            var lines = new StringBuilder[RowCount];
+            var maxLength = 0;
+            for (var i = 0; i < RowCount; i++)
+            {
+                lines[i] = new StringBuilder();
+                lines[i].Append("| ");
+                for (var j = 0; j < ColumnCount; j++)
+                    lines[i].Append(Values[i, j] + " ");
+                if (lines[i].Length > maxLength) maxLength = lines[i].Length;
+            }
+            var result = lines
+                .Select(line => line
+                    .Append(new string(' ', maxLength - line.Length) + "|"));
+            return string.Join("\n", result);
         }
     }
 }
--- C#/Solo/ExMath/Program.cs
+++ C#/Solo/ExMath/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace ExMath
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExMath
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var values1 = new double[,]
+            {
+                {1, 1}, 
+                {20, 20}
+            };
+            var values2 = new double[,]
+            {
+                {5, 2},
+                {3, 6}
+            };
+            Console.WriteLine(new Matrix(values1) * new Matrix(values2));
         }
     }
 }

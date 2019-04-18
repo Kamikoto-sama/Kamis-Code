021b15fb7efdbb31473ca353123095e4787f74f3
Kamikoto <dan.mok999@yandex.ru>
2019-04-18 15:19:43 +0500

    GUITest

---
+++ C#/Solo/GUITest/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace GUITest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.field = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // field
+            // 
+            this.field.Location = new System.Drawing.Point(1, 12);
+            this.field.Name = "field";
+            this.field.Size = new System.Drawing.Size(284, 20);
+            this.field.TabIndex = 0;
+            this.field.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 250);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.field);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox field;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ C#/Solo/UsefulExtensions/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsefulExtensions
+{
+    public static class Extensions
+    {
+        public static string Join<T>(this string separator, IEnumerable<T> values)
+            => string.Join(separator, values);
+
+        public static int ToInt(this string value)
+        {
+            int.TryParse(value, out var newValue);
+            return newValue;
+        }
+
+        public static int ToInt(this double value) => (int) value;
+
+        public static string ToPrint<T>(this IEnumerable<T> array)
+            => "{" + ", ".Join(array) + "}";
+
+        public static void Print(this string value) => Console.WriteLine(value);
+        
+        public static void Print<T>(this IEnumerable<T> array)
+            => Console.WriteLine(array.ToPrint());
+    }
+}

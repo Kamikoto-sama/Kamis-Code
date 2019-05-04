f1911ddae4a58bfe9d2bbd726db7523965c15993
Kamikoto <dan.mok999@yandex.ru>
2019-05-04 14:21:04 +0500

    new settings

--- C#/Solo/Test/Program.cs
+++ C#/Solo/Test/Program.cs
@@ -8,36 +8,11 @@ namespace Test
 {
     internal class Program
     {
+        
         public static void Main(string[] args)
         {
-            new B().M();
         }
-    }
-
-    class A
-    {
-        public A()
-        {
-            Console.WriteLine("A");
-        }
-
-        public void M()
-        {
-            Console.WriteLine(this + "A");
-        }
-    }
-
-    class B: A
-    {
-        public B()
-        {
-            Console.WriteLine("B");
-            M();
-        }
-
-        public void M()
-        {
-            base.M();   
-        }
+        
+        
     }
 }

3d451f994f3e60562f50850660111f701f049b3c
Kamikoto <dan.mok999@yandex.ru>
2019-05-31 16:18:41 +0500

    c#

--- C#/Solo/GUITest/MainForm.cs
+++ C#/Solo/GUITest/MainForm.cs
@@ -1,6 +1,9 @@
 using System;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GUITest
@@ -8,30 +11,51 @@ namespace GUITest
     public class MainForm: Form
     {
         private Label _label;
-        private TextBox _textBox;
         private Control.ControlCollection self;
+        private Button _button;
+        private ProgressBar _progress;
 
         public MainForm()
         {
-            _label = new Label();
-            _textBox = new TextBox
-            {
-                Size = new Size(Width, 50),
-                Location = new Point(0, _label.Bottom + 2)
-            };
-
             self = Controls;
+
+            _label = new Label{Size = new Size(Width - 20, 20)};
+            _button = new Button
+            {
+                Location = new Point(0, _label.Bottom),
+                Size = _label.Size
+            };
+            _progress = new ProgressBar
+            {
+                Location = new Point(0, _button.Bottom),
+                Size = _label.Size
+            };            
+            
             self.Add(_label);
-            self.Add(_textBox);
+            self.Add(_progress);
+            self.Add(_button);
+            _button.Click += (sender, args) => Work();
 
             this.StartPosition = FormStartPosition.CenterScreen;
-            KeyPress += Escape;
         }
 
-        private void Escape(object sender, KeyPressEventArgs args)
+        private void Work()
         {
-            if (args.KeyChar == (char)Keys.Escape)
-                this.Close();
+            var task = new BackgroundWorker{WorkerReportsProgress = true};
+            task.DoWork += DoWork;
+            task.RunWorkerCompleted += (sender, args) => _label.Text = "DONE";
+            task.ProgressChanged += (sender, args) => _progress.Value = args.ProgressPercentage;
+            task.RunWorkerAsync();
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            for (var i = 0; i < 100; i++)
+            {
+                Thread.Sleep(50);
+                worker.ReportProgress(i);
+            }
         }
     }
 }
--- C#/Solo/Test/Program.cs
+++ C#/Solo/Test/Program.cs
@@ -1,23 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
+using UsefulExtensions;
 
 namespace Test
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        [Flags]
+        public enum E
         {
-        }
-
-        private static void DoWork(int time)
-        {
-            Console.WriteLine($"Thread{time} started");
-            Thread.Sleep(time * 1000);
-            Console.WriteLine("Thread" + time);
+            First = 0,
+            Second = 1,
+            Third = 2
         }
         
+        public static void Main(string[] args)
+        {
+            var obj = Create(new T_T());
+            Console.WriteLine(obj);
+        }
+
+        private static T Create<T>(T tObj)
+        {
+            var type = typeof(T);
+            var properties = type.GetProperties();
+            foreach (var property in properties)
+            {
+                if (!property.PropertyType.IsClass) continue;
+                var valueCtor = property.PropertyType.GetConstructor(new Type[0]);
+                var value = valueCtor.Invoke(new object[0]);
+                property.SetValue(tObj, value);
+            }
+
+            return tObj;
+        }
+    }
+
+    class T_T
+    {
+        public int Count { get; set; }
+        public List<int> List { get; set; }
     }
 }

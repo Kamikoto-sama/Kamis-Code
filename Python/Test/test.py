37f4a9f7ec06c9835aff829d7ae567022c16fe0f
Kamikoto <dan.mok999@yandex.ru>
2019-10-01 19:04:24 +0500

    ABS py

--- Python/Test/test.py
+++ Python/Test/test.py
@@ -1,15 +1,4 @@
-from measurer import measurable
-from time import sleep
+from Abstarcts import abstract
 
-@measurable
-def a():
-	sleep(0.5)
-	print("1 done")
-
-@measurable(output=lambda x: print(round(x, 4)))
-def b():
-	sleep(0.5)
-	print("2")
-
-a()
-b()
+if __name__ == '__main__':
+    abstract(10)

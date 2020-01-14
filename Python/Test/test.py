901b771fb0a86325ef1c4590bb1a7fe87a7f7a9d
Kamiikoto <38423206+Kamikoto-sama@users.noreply.github.com>
2020-01-14 22:48:04 +0500

    Update test.py

--- Python/Test/test.py
+++ Python/Test/test.py
@@ -1,22 +1,10 @@
-from re import fullmatch
+def do(m, n):
+	if n > m:
+		m,n = n,m
+	while r := m % n:
+		m = n
+		n = r
+	return n
 
-def validate(number: str):
-	# number = prepare(number, "+() \t-", "")
-	pattern = r"((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}"
-	result = fullmatch(pattern, number)
-	if	result is None:
-		return False
-	# return f"+7 ({result.group(2)}) {result.group(3)}-{result.group(4)}-{result.group(5)}"
-	return True
-
-def prepare(sourceString, olds, new):
-	for i in olds:
-		sourceString = sourceString.replace(i, new)
-	return sourceString
-
-def test():
-	numbers = map(lambda x: x.strip(), open("numbers.txt").readlines())
-	for i in numbers:
-		print(i, "=", validate(i))
-		
-test()
+m,n = map(int, input().split())
+print(do(m,n))

8bc005fd689b53c47e1ab92ce92e6611c68ef19f
Kamikoto-sama <you@example.com>
2020-03-15 22:20:24 +0500

    C#

--- C#/Solo/Test/Program.cs
+++ C#/Solo/Test/Program.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization.Json;
-using System.Threading;
-using System.Threading.Tasks;
-using Useful;
+using System.Data.Entity;
 
 namespace Test
 {
@@ -18,16 +7,43 @@ namespace Test
     {
         public static void Main(string[] args)
         {
-            M();
+            using(UserContext db = new UserContext())
+            {
+                // создаем два объекта User
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Sam", Age = 26 };
+ 
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+ 
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users;
+                Console.WriteLine("Список объектов:");
+                foreach(User u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+            }
+            Console.Read();
         }
-
-        private static unsafe void M()
+    }
+    
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    
+    class UserContext : DbContext
+    {
+        public UserContext() : base("DbConnection")
         {
-            var a = "abc";
-            var b = "abc";
-            var x = &a;
-            var y = &x;
-            Console.WriteLine((uint) x);
         }
+          
+        public DbSet<User> Users { get; set; }
     }
 }

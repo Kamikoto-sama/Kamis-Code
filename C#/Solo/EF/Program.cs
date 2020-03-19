87e76389eb67d81d05b18d5b5bf364dcc432ad02
Kamiikoto <38423206+Kamikoto-sama@users.noreply.github.com>
2020-03-19 18:06:52 +0500

    чото там

---
+++ C#/Solo/EF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+
+	class AppContext: DbContext
+	{
+		public DbSet<User> Users { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; DataBase=TestDB; " +
+			                            "Trusted_Connection=True");
+		}
+	}
+	
+	class User
+	{
+		public int Id { get; set; }
+		public int Age { get; set; }
+		public string Name { get; set; }
+	}
+}

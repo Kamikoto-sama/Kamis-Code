274526cc82ddaf2cb5608cc62b4a9ee260e9d855
Kamiikoto <38423206+Kamikoto-sama@users.noreply.github.com>
2020-03-21 16:53:15 +0500

    fixes

--- C#/Solo/ASPCoreEmpty/Startup.cs
+++ C#/Solo/ASPCoreEmpty/Startup.cs
@@ -16,6 +16,8 @@ namespace ASPCoreEmpty
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddControllers();
+			services.AddRazorPages();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +29,8 @@ namespace ASPCoreEmpty
 			}
 
 			app.UseRouting();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-			});
+			app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+			app.Run(context => context.Response.WriteAsync("Page not fount :("));
 		}
 	}
 }
---
+++ C#/Solo/ASPCoreEmpty/Views/Main/RegisterForm.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPCoreEmpty.Views.Main
+{
+	public class RegisterForm : PageModel
+	{
+		public void OnGet()
+		{
+			
+		}
+	}
+}

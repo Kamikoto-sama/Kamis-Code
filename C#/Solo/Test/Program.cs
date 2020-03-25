712e13419226b91b772cc96c64ec6aea464295c5
Kamiikoto <38423206+Kamikoto-sama@users.noreply.github.com>
2020-03-25 12:10:19 +0500

    C#

--- C#/Solo/Test/Program.cs
+++ C#/Solo/Test/Program.cs
@@ -2,18 +2,29 @@
 
 namespace Test
 {
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-        }
-    }
-    
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-    
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			for (var i = 1; i <= 10; i++)
+				Console.WriteLine($"{i} -> {M(i)}");
+		}
+
+		private static int M(int n)
+		{
+			var step = n;
+			var counter = 0;
+
+			while (step > 0)
+			{
+				for (var i = step; i < n; i += 1)
+				for (var j = i; j >= step; j -= 1)
+				for (var l = 0; l < 1; l++)
+					counter++;
+				step /= 2;
+			}
+
+			return counter;
+		}
+	}
 }
